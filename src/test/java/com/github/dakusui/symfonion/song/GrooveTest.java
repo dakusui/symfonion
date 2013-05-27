6f701cbcc379a346c95e556c7585034d7070974b
hiroshi <dakusui@gmail.com>
2013-05-27 19:10:57 +0900

    Added test cases for groove calculation. Implemented parameter check in Groove#resolve.

--- src/test/java/com/github/dakusui/symfonion/song/GrooveTest.java
+++ src/test/java/com/github/dakusui/symfonion/song/GrooveTest.java
@@ -11,8 +11,10 @@ import com.github.dakusui.symfonion.core.Util;
 import static com.github.dakusui.symfonion.core.Util.parseNoteLength;
 
 public class GrooveTest {
+
 	@Test
 	public void test_A01() throws SymfonionException {
+		System.out.println("hello");
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
 		
@@ -21,6 +23,7 @@ public class GrooveTest {
 		assertEquals(1, unit.accentDelta);
 	}
 	
+	@Test
 	public void test_A02() throws SymfonionException {
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
@@ -30,6 +33,7 @@ public class GrooveTest {
 		assertEquals(0, unit.accentDelta);
 	}
 	
+	@Test
 	public void test_A03() throws SymfonionException {
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
@@ -38,24 +42,28 @@ public class GrooveTest {
 		assertEquals(48, unit.pos);
 		assertEquals(3, unit.accentDelta);
 	}
+
+	@Test
 	public void test_A04() throws SymfonionException {
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
 		
 		unit = groove.resolve(parseNoteLength("8."));
 		assertEquals(72, unit.pos);
-		assertEquals(1, unit.accentDelta);
+		assertEquals(4, unit.accentDelta);
 	}
 	
+	@Test
 	public void test_A05() throws SymfonionException {
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
 		
 		unit = groove.resolve(parseNoteLength("4"));
-		assertEquals(0, unit.pos);
-		assertEquals(1, unit.accentDelta);
+		assertEquals(96, unit.pos);
+		assertEquals(0, unit.accentDelta);
 	}
 	
+	@Test
 	public void test_A06() throws SymfonionException {
 		Groove groove = testGrooveA();
 		Groove.Unit unit;
@@ -74,12 +82,83 @@ public class GrooveTest {
 		return groove;
 	}
 	
+	@Test
+	public void test_B01() throws SymfonionException {
+		System.out.println("hello");
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(Fraction.zero);
+		assertEquals(0, unit.pos);
+		assertEquals(1, unit.accentDelta);
+	}
+	
+	@Test
+	public void test_B02() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(new Fraction(1, 32));
+		assertEquals(13, unit.pos);
+		assertEquals(0, unit.accentDelta);
+	}
+	
+	@Test
+	public void test_B03a() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(parseNoteLength("16"));
+		assertEquals(26, unit.pos);
+		assertEquals(2, unit.accentDelta);
+	}
+
+	@Test
+	public void test_B03b() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(parseNoteLength("8"));
+		assertEquals(48, unit.pos);
+		assertEquals(3, unit.accentDelta);
+	}
+
+	@Test
+	public void test_B04() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(parseNoteLength("8."));
+		assertEquals(73, unit.pos);
+		assertEquals(4, unit.accentDelta);
+	}
+	
+	@Test
+	public void test_B05() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(parseNoteLength("4"));
+		assertEquals(96, unit.pos);
+		assertEquals(0, unit.accentDelta);
+	}
+	
+	@Test
+	public void test_B06() throws SymfonionException {
+		Groove groove = testGrooveB();
+		Groove.Unit unit;
+		
+		unit = groove.resolve(parseNoteLength("2"));
+		assertEquals(192, unit.pos);
+		assertEquals(0, unit.accentDelta);
+	}
+
 	Groove testGrooveB() throws SymfonionException {
 		Groove groove = new Groove();
 		groove.add(Util.parseNoteLength("16"), 26, 1);
 		groove.add(Util.parseNoteLength("16"), 22, 2);
 		groove.add(Util.parseNoteLength("16"), 25, 3);
-		groove.add(Util.parseNoteLength("16"), 24, 4);
+		groove.add(Util.parseNoteLength("16"), 23, 4);
 		return groove;
 	}
 }
