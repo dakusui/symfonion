405a20495030ebd675819fdb70972a20a1bb1065
hiroshi <dakusui@gmail.com>
2013-05-27 19:15:13 +0900

    Emphasized groove in example4.js

--- src/main/resources/example4.js
+++ src/main/resources/example4.js
@@ -28,30 +28,24 @@
 	"$grooves":{
 		"16beats":[
 		    { "$length":"16", "$ticks":28, "$accent":30 },
-		    { "$length":"16", "$ticks":20, "$accent":0 },
+		    { "$length":"16", "$ticks":20, "$accent":-10 },
 		    { "$length":"16", "$ticks":26, "$accent":10 },
-		    { "$length":"16", "$ticks":22, "$accent":0},
+		    { "$length":"16", "$ticks":22, "$accent":-5},
 		    { "$length":"16", "$ticks":28, "$accent":20 },
-		    { "$length":"16", "$ticks":20, "$accent":0 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
 		    { "$length":"16", "$ticks":26, "$accent":10 },
-		    { "$length":"16", "$ticks":22, "$accent":0 },
+		    { "$length":"16", "$ticks":22, "$accent":-4 },
 		    { "$length":"16", "$ticks":28, "$accent":25 },
-		    { "$length":"16", "$ticks":20, "$accent":0 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
 		    { "$length":"16", "$ticks":26, "$accent":10 },
-		    { "$length":"16", "$ticks":22, "$accent":0 },
+		    { "$length":"16", "$ticks":22, "$accent":-5 },
 		    { "$length":"16", "$ticks":28, "$accent":15 },
-		    { "$length":"16", "$ticks":20, "$accent":0 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
 		    { "$length":"16", "$ticks":26, "$accent":10 },
-		    { "$length":"16", "$ticks":22, "$accent":0 }
+		    { "$length":"16", "$ticks":22, "$accent":-10 }
 		]
 	},
 	"$sequence":[
-  	    {
-			"$beats":"1/4",
-			"$patterns":{
-				"vocal":["setting"]
-			}
-		},
 	    {
 			"$beats":"16/16",
 			"$patterns":{
