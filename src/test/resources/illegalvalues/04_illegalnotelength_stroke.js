ad2b903a08be26418470ee5f78a4d5c045d30af9
hiroshi <dakusui@gmail.com>
2013-06-09 06:35:11 +0900

    Implemented tests for error handlings.

---
+++ src/test/resources/illegalvalues/04_illegalnotelength_stroke.js
@@ -0,0 +1,64 @@
+{
+	"$parts":{
+		"vocal":{ "$channel":0, "$port":"port1" }
+	},
+	"$notemaps":{
+	},
+	"$patterns":{
+		"melody1":{
+		    "$notemap":"$normal",
+		    "$body":[
+		             "C", "D", "E", "F", "C", "D", "E","F",
+		             "C", "D", "E", "F", "C", "D", "E",{"$notes":"C", "$length":"1/2"}
+		    ],
+		    "$parameters":{
+		        "$velocitybase":106,
+		        "$velocitydelta":10,
+		        "$gate":0.8,
+		        "$length":"16",
+		        "$transpose":0
+		    }
+		},
+		"setting":{
+			"$body":[
+			     {"$tempo":120, "$reverb":127, "$chorus":127},
+			]
+		}
+	},
+	"$grooves":{
+		"16beats":[
+		    { "$length":"16", "$ticks":28, "$accent":30 },
+		    { "$length":"16", "$ticks":20, "$accent":-10 },
+		    { "$length":"16", "$ticks":26, "$accent":10 },
+		    { "$length":"16", "$ticks":22, "$accent":-5},
+		    { "$length":"16", "$ticks":28, "$accent":20 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
+		    { "$length":"16", "$ticks":26, "$accent":10 },
+		    { "$length":"16", "$ticks":22, "$accent":-4 },
+		    { "$length":"16", "$ticks":28, "$accent":25 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
+		    { "$length":"16", "$ticks":26, "$accent":10 },
+		    { "$length":"16", "$ticks":22, "$accent":-5 },
+		    { "$length":"16", "$ticks":28, "$accent":15 },
+		    { "$length":"16", "$ticks":20, "$accent":-8 },
+		    { "$length":"16", "$ticks":26, "$accent":10 },
+		    { "$length":"16", "$ticks":22, "$accent":-10 }
+		]
+	},
+	"$sequence":[
+	    {
+			"$beats":"16/16",
+			"$patterns":{
+				"vocal":["melody1"]
+			},
+			"$groove":"16beats"
+		},
+	    {
+			"$beats":"16/16",
+			"$patterns":{
+				"vocal":["melody1"]
+			},
+			"$groove":"16beats"
+		}
+	]
+}
