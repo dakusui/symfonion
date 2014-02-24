dc28e18be1b593650ec8c5769d262d98b9594282
Hiroshi Ukai <dakusui@gmail.com>
2014-02-24 18:48:03 +0900

    Implement tests for promotion mechanism.

--- src/main/java/com/github/dakusui/json/JsonTypeMismatchException.java
+++ src/main/java/com/github/dakusui/json/JsonTypeMismatchException.java
@@ -1,14 +1,14 @@
 package com.github.dakusui.json;
 
-import java.util.Arrays;
-
 import com.github.dakusui.json.JsonUtil.JsonTypes;
 import com.google.gson.JsonElement;
+
+import java.util.Arrays;
 
 /**
  * An exception thrown when a Json element is found on a certain path has an
  * invalid type.
- *  
+ * 
  * @author hiroshi
  */
 public class JsonTypeMismatchException extends JsonException {
@@ -26,7 +26,7 @@ public class JsonTypeMismatchException extends JsonException {
 	 * A string that describes the reason why the element is considered invalid.
 	 */
 	private String reason;
-	
+
 	/**
 	 * Creates an object of this class.
 	 * 
@@ -44,9 +44,9 @@ public class JsonTypeMismatchException extends JsonException {
 	 * @param reason A string that describes the reason why <code>elem</code> was considered invalid.
 	 */
 	public JsonTypeMismatchException(JsonElement elem, String reason) {
-		this(elem, reason, new JsonTypes[]{});
+		this(elem, reason, new JsonTypes[] {});
 	}
-	
+
 	/**
 	 * Creates an object of this class.
 	 * 
@@ -67,7 +67,7 @@ public class JsonTypeMismatchException extends JsonException {
 		String ret = null;
 		String r = "";
 		if (reason != null) {
-			r = String.format("(%s)");
+			r = String.format("(%s)", reason);
 		}
 		if (types == null || types.length == 0) {
 			ret = String.format("%s is not allowed here%s.", r, summary(actualJSON));
@@ -76,7 +76,7 @@ public class JsonTypeMismatchException extends JsonException {
 		}
 		return ret;
 	}
-	
+
 	/**
 	 * Returns a formatted message.
 	 */
--- src/test/java/com/github/dakusui/json/PromotionMechanismTest.java
+++ src/test/java/com/github/dakusui/json/PromotionMechanismTest.java
@@ -1,44 +1,107 @@
 package com.github.dakusui.json;
 
+import com.github.dakusui.jcunit.core.BasicSummarizer;
+import com.github.dakusui.jcunit.core.DefaultRuleSetBuilder;
+import com.github.dakusui.jcunit.core.Generator;
+import com.github.dakusui.jcunit.core.In;
+import com.github.dakusui.jcunit.core.In.Domain;
+import com.github.dakusui.jcunit.core.JCUnit;
+import com.github.dakusui.jcunit.core.Out;
+import com.github.dakusui.jcunit.core.RuleSet;
+import com.github.dakusui.jcunit.core.Summarizer;
+import com.github.dakusui.jcunit.generators.PairwiseTestArrayGenerator;
+import com.github.dakusui.symfonion.core.SymfonionException;
+import com.github.dakusui.symfonion.core.Util;
 import com.google.gson.JsonObject;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JCUnit.class)
+@Generator(PairwiseTestArrayGenerator.class)
 public class PromotionMechanismTest {
-	//@Test
-	public void test() throws JsonPathNotFoundException, JsonInvalidPathException, JsonTypeMismatchException  {
-		JsonUtil.asJsonObjectWithPromotion(base(), prioritizedKeys(), path());
-	}
-	
-	private static  Object path() {
-		// TODO Auto-generated method stub
-		return null;
+	@Rule
+	public RuleSet rules = new DefaultRuleSetBuilder().autoRuleSet(this).summarizer(summarizer);
+
+	@ClassRule
+	public static Summarizer summarizer = new BasicSummarizer();
+
+	@In(domain = Domain.Method)
+	public Object[] pathToParent;
+
+	public static Object[][] pathToParent() {
+		return new Object[][] {
+				new Object[] {},
+				new Object[] { "non-existing" },
+				new Object[] { "level1" },
+				new Object[] { "level1", "non-existing" },
+				new Object[] { "level1", "level2" },
+		};
 	}
 
-	private static String[] prioritizedKeys() {
-		return new String[]{"key1", "key2"};
+	@In(domain = Domain.Method)
+	public Object key;
+
+	public static Object[] key() {
+		return new Object[] {
+				"key-null-0",
+				"key-prim-0",
+				"key-arr-0",
+				"key-arr-1",
+				"key-arr-2",
+				"key-arr-3",
+				"key-obj-0",
+				"key-obj-1",
+				"key-obj-2",
+				"key-obj-3",
+		};
 	}
-	
-	private static JsonObject base() {
-		JsonObject obj = JsonUtil.toJson(""
-/*
-{
-    "primitive":"hello",
-    "array":["hello", "world"],
-    "null":null,
-    "object":{
-        "key1":"hello",
-        "key2":"world"
-    },
-    "path1":{
-	    "primitive":"hello",
-	    "array":["hello", "world"],
-	    "null":null,
-	    "object":{
-	        "key1":"hello",
-	        "key2":"world"
-    }
-}
-*/
-		).getAsJsonObject();
+
+	@In(domain = Domain.Method)
+	public String[] prioritizedKeys;
+
+	@Out
+	public JsonObject obj;
+
+	@Out
+	public String exceptionMessage;
+
+	@Out
+	public Class<? extends Exception> exceptionClass;
+
+	public static String[][] prioritizedKeys() {
+		return new String[][] {
+				new String[] {},
+				new String[] { "key-null-0" },
+				new String[] { "key-prim-0", "key-null-0" },
+				new String[] { "key-prim-0", "key-arr-1", "key-obj-1" },
+		};
+	}
+
+	private static JsonObject base() throws SymfonionException {
+		JsonObject obj = JsonUtil.toJson(
+				Util.loadResource(PromotionMechanismTest.class.getCanonicalName().replaceAll("\\.", "/") + ".js")
+				).getAsJsonObject();
 		return obj;
+	}
+
+	@Test
+	public void test() throws Exception {
+		this.obj = null;
+		try {
+			this.obj = JsonUtil.asJsonObjectWithPromotion(base(), prioritizedKeys, path(pathToParent, key));
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			this.exceptionClass = e.getClass();
+			this.exceptionMessage = e.getMessage();
+		}
+	}
+
+	private static Object[] path(Object[] pathToParent, Object key) {
+		return ArrayUtils.add(pathToParent, key);
 	}
 }
