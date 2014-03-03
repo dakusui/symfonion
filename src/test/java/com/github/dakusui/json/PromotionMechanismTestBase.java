983bb2fc410a640cf8a4b75d779826a906034f73
hiroshi <dakusui@gmail.com>
2014-03-03 09:04:56 +0900

    Priliminary commit for improving promotion mechanism test.

---
+++ src/test/java/com/github/dakusui/json/PromotionMechanismTestBase.java
@@ -0,0 +1,103 @@
+package com.github.dakusui.json;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.github.dakusui.jcunit.core.BasicSummarizer;
+import com.github.dakusui.jcunit.core.DefaultRuleSetBuilder;
+import com.github.dakusui.jcunit.core.In;
+import com.github.dakusui.jcunit.core.In.Domain;
+import com.github.dakusui.jcunit.core.JCUnit;
+import com.github.dakusui.jcunit.core.Out;
+import com.github.dakusui.jcunit.core.RuleSet;
+import com.github.dakusui.jcunit.core.Summarizer;
+import com.github.dakusui.jcunit.core.SystemProperties;
+import com.github.dakusui.symfonion.core.SymfonionException;
+import com.github.dakusui.symfonion.core.Util;
+import com.google.gson.JsonObject;
+
+@RunWith(JCUnit.class)
+public abstract class PromotionMechanismTestBase {
+  static {
+    SystemProperties.jcunitBaseDir("src/test/resources/jcunit");
+  }
+
+  @Rule
+  public RuleSet           rules      = new DefaultRuleSetBuilder()
+                                          .autoRuleSet(this).summarizer(
+                                              summarizer);
+
+  @ClassRule
+  public static Summarizer summarizer = new BasicSummarizer();
+
+  @In(domain = Domain.Method)
+  public JsonObject        base;
+
+  @In(domain = Domain.Method)
+  public Object[]          pathToParent;
+
+  public static Object[][] pathToParent() {
+    return new Object[][] { new Object[] {}, new Object[] { "non-existing" },
+        new Object[] { "level1" }, new Object[] { "level1", "non-existing" },
+        new Object[] { "level1", "level2" }, };
+  }
+
+  @In(domain = Domain.Method)
+  public Object key;
+
+  public static Object[] key() {
+    return new Object[] { "key-null-0", "key-prim-0", "key-arr-0", "key-arr-1",
+        "key-arr-2", "key-arr-3", "key-obj-0", "key-obj-1", "key-obj-2",
+        "key-obj-3", };
+  }
+
+  @In(domain = Domain.Method)
+  public String[]                   prioritizedKeys;
+
+  @Out
+  public JsonObject                 obj;
+
+  @Out
+  public String                     exceptionMessage;
+
+  @Out
+  public Class<? extends Exception> exceptionClass;
+
+  public static String[][] prioritizedKeys() {
+    return new String[][] { new String[] { "key1" },
+        new String[] { "key1", "key2" },
+        new String[] { "key1", "key2", "key3" }, new String[] {}, };
+  }
+
+  public static JsonObject[] base() {
+    try {
+      JsonObject obj = JsonUtil.toJson(
+          Util.loadResource(PromotionMechanismTestBase.class.getCanonicalName()
+              .replaceAll("\\.", "/") + ".js")).getAsJsonObject();
+      return new JsonObject[] { obj };
+    } catch (SymfonionException e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  }
+
+  @Test
+  public void test() throws Exception {
+    this.obj = null;
+    try {
+      this.obj = JsonUtil.asJsonObjectWithPromotion(base, prioritizedKeys,
+          path(pathToParent, key));
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (JsonException e) {
+      this.exceptionClass = e.getClass();
+      this.exceptionMessage = e.getMessage();
+    }
+  }
+
+  private static Object[] path(Object[] pathToParent, Object key) {
+    return ArrayUtils.add(pathToParent, key);
+  }
+}
