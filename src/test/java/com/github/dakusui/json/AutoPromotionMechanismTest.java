1c804429f1aa70a087882e9b816a820952ca9395
hiroshi <dakusui@gmail.com>
2014-03-04 07:10:22 +0900

    Remove old test case and test data.

--- src/test/java/com/github/dakusui/json/AutoPromotionMechanismTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.dakusui.json;
-
-import com.github.dakusui.jcunit.core.Generator;
-import com.github.dakusui.jcunit.generators.SimpleTestArrayGenerator;
-
-@Generator(SimpleTestArrayGenerator.class)
-public class AutoPromotionMechanismTest extends PromotionMechanismTestBase {
-
-}
