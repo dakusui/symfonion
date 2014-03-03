d1594025cb736db14e9d9673b16f15ed9e3688be
hiroshi <dakusui@gmail.com>
2014-03-03 09:04:56 +0900

    Priliminary commit for improving promotion mechanism test.

---
+++ src/test/java/com/github/dakusui/json/AutoPromotionMechanismTest.java
@@ -0,0 +1,9 @@
+package com.github.dakusui.json;
+
+import com.github.dakusui.jcunit.core.Generator;
+import com.github.dakusui.jcunit.generators.SimpleTestArrayGenerator;
+
+@Generator(SimpleTestArrayGenerator.class)
+public class AutoPromotionMechanismTest extends PromotionMechanismTestBase {
+
+}
