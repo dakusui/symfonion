cad44606b68fc5b241f3918066069589cd6e9f92
hiroshi <dakusui@gmail.com>
2014-03-04 07:09:32 +0900

    Add pairwise policy tests.

---
+++ src/test/java/com/github/dakusui/json/PromotionMechanismAutoTest.java
@@ -0,0 +1,9 @@
+package com.github.dakusui.json;
+
+import com.github.dakusui.jcunit.core.Generator;
+import com.github.dakusui.jcunit.generators.PairwiseTestArrayGenerator;
+
+@Generator(PairwiseTestArrayGenerator.class)
+public class PromotionMechanismAutoTest extends PromotionMechanismTestBase {
+
+}
