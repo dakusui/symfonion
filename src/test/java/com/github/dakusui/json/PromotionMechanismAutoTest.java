bd9b88e23a162036b2cdd2529b4a28e068a79290
Hiroshi Ukai <dakusui@gamil.com>
2023-12-24 06:54:27 +0900

    Clean pom.xml.

--- src/test/java/com/github/dakusui/json/PromotionMechanismAutoTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.dakusui.json;
-
-import com.github.dakusui.jcunit.core.Generator;
-import com.github.dakusui.jcunit.generators.PairwiseTestArrayGenerator;
-
-@Generator(PairwiseTestArrayGenerator.class)
-public class PromotionMechanismAutoTest extends PromotionMechanismTestBase {
-
-}
