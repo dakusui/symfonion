a5a0cb57f6d216c36fe6d89915fe075f92dac874
Hiroshi Ukai <dakusui@gamil.com>
2024-01-02 09:36:32 +0900

    Issue-#21: Ignore PatchBay test in environments where necessary devices are not found.

--- src/test/java/com/github/dakusui/symfonion/tests/cli/subcommands/PatchBayTest.java
+++ src/test/java/com/github/dakusui/symfonion/tests/cli/subcommands/PatchBayTest.java
@@ -3,12 +3,14 @@ package com.github.dakusui.symfonion.tests.cli.subcommands;
 import com.github.dakusui.symfonion.cli.Cli;
 import com.github.dakusui.symfonion.cli.subcommands.PatchBay;
 import com.github.dakusui.testutils.TestUtils;
+import com.github.dakusui.testutils.midi.MidiTestUtils;
 import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static com.github.dakusui.testutils.TestUtils.immediatelyClosingInputStream;
+import static com.github.dakusui.testutils.midi.MidiTestUtils.assumeMidiDevicesPresent;
 import static com.github.dakusui.thincrest.TestAssertions.assertThat;
 import static com.github.dakusui.thincrest_pcond.experimentals.cursor.Cursors.findElements;
 import static com.github.dakusui.thincrest_pcond.forms.Predicates.containsString;
@@ -16,6 +18,7 @@ import static com.github.dakusui.thincrest_pcond.forms.Predicates.containsString
 public class PatchBayTest {
   @Test
   public void whenHelp_thenLooksOk() throws ParseException, IOException {
+    assumeMidiDevicesPresent();
     TestUtils.OutputCapturingPrintStream out = TestUtils.outputCapturingPrintStream();
     new PatchBay().invoke(new Cli("-r", "in=out", "-I", "in=Real.*", "-O", "out=Gervill"), out, immediatelyClosingInputStream());
 
--- src/test/java/com/github/dakusui/testutils/midi/MidiTestUtils.java
+++ src/test/java/com/github/dakusui/testutils/midi/MidiTestUtils.java
@@ -1,11 +1,31 @@
 package com.github.dakusui.testutils.midi;
 
+import org.junit.AssumptionViolatedException;
+
+import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 import static com.github.dakusui.testutils.TestUtils.toHex;
+import static java.util.Arrays.asList;
 
 public class MidiTestUtils {
   public static String formatMidiMessage(MidiMessage message) {
     return String.format("message: %-20s: %s", message.getClass().getSimpleName(), toHex(message.getMessage()));
+  }
+
+  public static void assumeMidiDevicesPresent() {
+    MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+    List<String> missingDevices = new LinkedList<>();
+    for (String requiredDeviceName : asList("Gervill", "Real Time Sequencer"))
+      if (Arrays.stream(infos).noneMatch(i -> Objects.equals(requiredDeviceName, i.getName())))
+        missingDevices.add(requiredDeviceName);
+    if (!missingDevices.isEmpty())
+      throw new AssumptionViolatedException("MIDI-devices: " + missingDevices + " were not found in this system. Known devices are: " + Arrays.toString(infos));
   }
 }
