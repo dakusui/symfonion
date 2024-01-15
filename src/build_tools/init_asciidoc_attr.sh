dafdf3440ef6bd93c838318814ba489e8968605f
Hiroshi Ukai <dakusui@gamil.com>
2024-01-16 01:10:02 +0900

    Issue-42: Design mmml wrapper for symfonion (WIP)

--- src/build_tools/init_asciidoc_attr.sh
+++ src/build_tools/init_asciidoc_attr.sh
@@ -11,10 +11,13 @@ doc_base="${1:-${top_dir}}"
 # shellcheck disable=SC2156
 find "${top_dir}" \
   -type d -not -path "${top_dir}" \
-  -exec sh -c "echo 'include::../.attr.adoc[]' > {}/.attr.adoc" {} \;
+  -exec sh -c "echo '
+### CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT ###
+include::../.attr.adoc[]' > {}/.attr.adoc" {} \;
 
 _target_file="src/site/asciidoc/example-project/.attr.adoc"
 echo "
+### CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT ###
 :doc_base: ${doc_base}
 :project_root: ${doc_base}/home/USER/WORKSPACE
 :project_src: ${doc_base}/home/USER/WORKSPACE/src
@@ -27,6 +30,7 @@ if [[ "${doc_base}" == "${top_dir}" ]]; then
   echo "
 [.text-right]
 **<link:{doc_base}/INDEX.adoc[top]>**
+**<link:{doc_base}/home/USER/WORKSPACE/example-song/INDEX.adoc[top]>**
 **<link:${myself}[update .attr base]>**
 
 [.text-left]
