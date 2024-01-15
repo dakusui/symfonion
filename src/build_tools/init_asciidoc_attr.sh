d1e7b203367691999693ed1f4d73d813da65a2f2
Hiroshi Ukai <dakusui@gamil.com>
2024-01-16 01:15:34 +0900

    Issue-42: Design mmml wrapper for symfonion (WIP)

--- src/build_tools/init_asciidoc_attr.sh
+++ src/build_tools/init_asciidoc_attr.sh
@@ -11,13 +11,11 @@ doc_base="${1:-${top_dir}}"
 # shellcheck disable=SC2156
 find "${top_dir}" \
   -type d -not -path "${top_dir}" \
-  -exec sh -c "echo '
-### CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT ###
-include::../.attr.adoc[]' > {}/.attr.adoc" {} \;
+  -exec sh -c "printf '%s\n%s' '// CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DO NOT EDIT //' 'include::../.attr.adoc[]' > {}/.attr.adoc" {} \;
 
 _target_file="src/site/asciidoc/example-project/.attr.adoc"
 echo "
-### CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT ###
+// CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT //
 :doc_base: ${doc_base}
 :project_root: ${doc_base}/home/USER/WORKSPACE
 :project_src: ${doc_base}/home/USER/WORKSPACE/src
