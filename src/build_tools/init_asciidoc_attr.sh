edba1140d194b24ac1ac68875525c4823451fca8
Hiroshi Ukai <dakusui@gamil.com>
2024-01-29 15:02:08 +0900

    Issue-#42: Design mmml wrapper for symfonion (WIP)

--- src/build_tools/init_asciidoc_attr.sh
+++ src/build_tools/init_asciidoc_attr.sh
@@ -7,7 +7,7 @@ shopt -s inherit_errexit
   exit 1
 }
 myself="$(realpath "${0}")"
-top_dir="$(pwd)/src/site/mmml/directory-layout"
+top_dir="$(pwd)/src/site/mmml"
 doc_base="${1:-${top_dir}}"
 
 # shellcheck disable=SC2156
@@ -19,29 +19,31 @@ _target_file="${top_dir}/.attr.adoc"
 echo "
 // CAUTION: This file is auto-generated. Add .attr.adoc to .gitignore and DON'T EDIT //
 :doc_base: ${doc_base}
-:project_root: ${doc_base}/home/USER/WORKSPACE
-:project_src: ${doc_base}/home/USER/WORKSPACE/src
-:main_mmml_src: ${doc_base}/home/USER/WORKSPACE/src/mmml
-:target_dir: ${doc_base}/home/USER/WORKSPACE/target
-:target_mmml_dir: ${doc_base}/home/USER/WORKSPACE/target/mmml
-:xdg_config_home: ${doc_base}/home/USER/.config
-:xdg_data_home: ${doc_base}/home/USER/.local/share
-:xdg_local_bin: ${doc_base}/home/USER/.local/bin
-:xdg_local_lib: ${doc_base}/home/USER/.local/lib
+:directory-layout: {doc_base}/directory-layout
+:project_root: {directory-layout}/home/USER/WORKSPACE/example-song
+:project_src: {project_root}/src
+:main_mmml_src: {project_root}/src/mmml
+:target_dir: {project_root}/target
+:target_mmml_dir: {project_root}/target/mmml
+:xdg_config_home: {directory-layout}/home/USER/.config
+:xdg_data_home: {directory-layout}/home/USER/.local/share
+:xdg_local_bin: {directory-layout}/home/USER/.local/bin
+:xdg_local_lib: {directory-layout}/home/USER/.local/lib
 " > "${_target_file}"
 
 if [[ "${doc_base}" == "${top_dir}" ]]; then
   echo "
 [.text-right]
-**<link:{doc_base}/INDEX.adoc[top]>**
-**<link:{doc_base}/home/USER/WORKSPACE/example-song/INDEX.adoc[project_root]>**
+**<link:{directory-layout}/INDEX.adoc[top]>**
+**<link:{project_root}/INDEX.adoc[project_root]>**
 **<link:${myself}[update .attr base]>**
 [.text-right]
-**<link:{xdg_config_home}/mmml/INDEX.adoc[XDG_CONFIG_HOME/mmml]>**
-**<link:{xdg_data_home}/mmml/INDEX.adoc[XDG_DATA_HOME/mmml]>**
-**<link:{xdg_local_bin}/INDEX.adoc[XDG_LOCAL_BIN]>**
-**<link:{xdg_local_lib}/mmml/INDEX.adoc[XDG_LOCAL_LIB/mmml]>**
-
+- **<link:{xdg_config_home}/mmml/INDEX.adoc[XDG_CONFIG_HOME/mmml: +{xdg_config_home}+/mmml/INDEX.adoc]>**
+- **<link:{xdg_data_home}/mmml/INDEX.adoc[XDG_DATA_HOME/mmml: +{xdg_data_home}+/mmml/INDEX.adoc]>**
+- **<link:{xdg_local_bin}/INDEX.adoc[XDG_LOCAL_BIN: +{xdg_local_bin}+/INDEX.adoc]>**
+- **<link:{xdg_local_lib}/mmml/INDEX.adoc[XDG_LOCAL_LIB/mmml: +{xdg_local_lib}+/mmml/INDEX.adoc]>**
+- ** link:{doc_base}/features/mmml-pipeline.adoc[+{doc_base}+/features/mmml-pipeline.adoc]**
+- ** link:{xdg_local_lib}/mmml/1.0.1/lib/json{plus}{plus}[+{xdg_local_lib}+/mmml/1.0.1/lib/json++]**
 [.text-left]
 " >> "${_target_file}"
 fi
