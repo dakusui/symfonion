2e62f7dd8f934b89748c8e76f2d8dc47a756cc07
Hiroshi Ukai <dakusui@gamil.com>
2024-12-09 08:14:48 +0900

    Refactor Bar class and add some JavaDoc

--- src/main/java/com/github/dakusui/symfonion/song/Bar.java
+++ src/main/java/com/github/dakusui/symfonion/song/Bar.java
@@ -1,10 +1,10 @@
 package com.github.dakusui.symfonion.song;
 
-import com.github.dakusui.symfonion.compat.json.CompatJsonException;
-import com.github.dakusui.symfonion.compat.json.JsonInvalidPathException;
-import com.github.dakusui.symfonion.compat.json.CompatJsonUtils;
 import com.github.dakusui.symfonion.compat.exceptions.FractionFormatException;
 import com.github.dakusui.symfonion.compat.exceptions.SymfonionException;
+import com.github.dakusui.symfonion.compat.json.CompatJsonException;
+import com.github.dakusui.symfonion.compat.json.CompatJsonUtils;
+import com.github.dakusui.symfonion.compat.json.JsonInvalidPathException;
 import com.github.dakusui.symfonion.utils.Fraction;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -15,35 +15,41 @@ import java.util.Map.Entry;
 import java.util.function.Predicate;
 import java.util.stream.StreamSupport;
 
-import static com.github.dakusui.symfonion.compat.json.CompatJsonUtils.asJsonElement;
 import static com.github.dakusui.symfonion.compat.exceptions.CompatExceptionThrower.*;
 import static com.github.dakusui.symfonion.compat.exceptions.CompatExceptionThrower.ContextKey.JSON_ELEMENT_ROOT;
 import static com.github.dakusui.symfonion.compat.exceptions.CompatExceptionThrower.ContextKey.REFERENCING_JSON_NODE;
 import static com.github.dakusui.symfonion.compat.exceptions.SymfonionIllegalFormatException.FRACTION_EXAMPLE;
 import static com.github.dakusui.symfonion.compat.exceptions.SymfonionTypeMismatchException.ARRAY;
-import static java.util.Collections.singletonList;
+import static com.github.dakusui.symfonion.compat.json.CompatJsonUtils.asJsonElement;
+import static com.github.dakusui.symfonion.utils.Fraction.parseFraction;
+import static java.util.Collections.*;
 
+/**
+ * A class that models a "bar" in a musical score.
+ */
 public class Bar {
   private final Map<String, Pattern> patterns;
   private final JsonObject rootJsonObject;
   private final Map<String, NoteMap> noteMaps;
-  final Fraction beats;
-  final Map<String, List<List<Pattern>>> patternLists = new HashMap<>();
-  final Groove groove;
-  final List<String> labels;
+  private final Fraction beats;
+  private final Map<String, List<List<Pattern>>> patternLists;
+  private final Groove groove;
+  private final List<String> labels;
   private final JsonObject barJsonObject;
 
 
   /**
    * Creates a `Bar` object.
    *
+   * `root` is used only for composing messages on errors.
+   *
    * @param barJsonObject A JSON object from which a bar is created.
-   * @param root A root JSON object that `barJsonObject` belongs to.
-   * @param grooves A map that defines a groove on which this bar should be played.
-   * @param noteMaps A note map with which this bar should be played.
-   * @param patterns A map that holds that defines patterns this bar references.
-   * @param partFilter A predicate that filters parts to be played in this bar.
-   * @throws SymfonionException Data processing was failed.
+   * @param root          A root JSON object that `barJsonObject` belongs to.
+   * @param grooves       A map that defines a groove on which this bar should be played.
+   * @param noteMaps      A note map with which this bar should be played.
+   * @param patterns      A map that holds that defines patterns this bar references.
+   * @param partFilter    A predicate that filters parts to be played in this bar.
+   * @throws SymfonionException  Data processing was failed.
    * @throws CompatJsonException Invalid JSON is given.
    */
   public Bar(JsonObject barJsonObject, JsonObject root, Map<String, Groove> grooves, Map<String, NoteMap> noteMaps, Map<String, Pattern> patterns, Predicate<String> partFilter) throws SymfonionException, CompatJsonException {
@@ -59,28 +65,90 @@ public class Bar {
         partName -> [ patternName ]
        */
       JsonObject patternsJsonObjectInBar = getPartsInBarAsJsonObject(barJsonObject);
-      for (Entry<String, JsonElement> patternEntryJsonElement : patternsJsonObjectInBar.entrySet()) {
-        if (!partFilter.test(patternEntryJsonElement.getKey()))
-          continue;
-        List<List<Pattern>> patternLists = new LinkedList<>();
-        String partName = patternEntryJsonElement.getKey();
-        JsonArray partPatternsJsonArray = getPatternsForPartJsonElements(partName, patternsJsonObjectInBar);
-        for (JsonElement jsonPatterns : partPatternsJsonArray) {
-          String patternName = jsonPatterns.getAsString();
-          try (Context ignored2 = context($(REFERENCING_JSON_NODE, jsonPatterns))) {
-            patternLists.add(createPattern(patternName));
-          }
-        }
-        this.patternLists.put(partName, patternLists);
-      }
+      this.patternLists = composePatternListMap(this, partFilter, patternsJsonObjectInBar);
     }
   }
 
-  private List<String> resolveLabelsForBar(JsonObject barJsonObject) {
-    if (CompatJsonUtils.hasPath(barJsonObject, Keyword.$labels)) {
-      return StreamSupport.stream(CompatJsonUtils.asJsonArray(barJsonObject, Keyword.$labels).spliterator(), false).map(CompatJsonUtils::asString).toList();
+  /**
+   * Returns labels attached to this bar object.
+   *
+   * @return A list of labels.
+   */
+  public List<String> labels() {
+    return this.labels;
+  }
+
+  /**
+   * Returns a set of part names.
+   *
+   * @return A set of part names.
+   */
+  public Set<String> partNames() {
+    return unmodifiableSet(this.patternLists.keySet());
+  }
+
+  /**
+   * Returns a list of a list of patterns for the `partName`.
+   * TODO
+   *
+   * @param partName A part name for which patterns should be returned.
+   * @return A list of a list of patterns.
+   */
+  public List<List<Pattern>> part(String partName) {
+    return unmodifiableList(this.patternLists.get(partName));
+  }
+
+  public Fraction beats() {
+    return this.beats;
+  }
+
+  public Groove groove() {
+    return this.groove;
+  }
+
+  public JsonElement lookUpJsonNode(String partName) {
+    try {
+      return asJsonElement(this.barJsonObject, Keyword.$parts, partName);
+    } catch (JsonInvalidPathException e) {
+      return null;
     }
-    return Collections.emptyList();
+  }
+
+  /**
+   * Returns a root JSON object to which this bar belongs.
+   *
+   * @return A root JSON object.
+   */
+  public JsonObject rootJsonObject() {
+    return this.rootJsonObject;
+  }
+
+  private static Map<String, List<List<Pattern>>> composePatternListMap(Bar bar, Predicate<String> partFilter, JsonObject patternsJsonObjectInBar) {
+    Map<String, List<List<Pattern>>> patternListsMap = new HashMap<>();
+    for (Entry<String, JsonElement> patternEntryJsonElement : patternsJsonObjectInBar.entrySet()) {
+      if (!partFilter.test(patternEntryJsonElement.getKey()))
+        continue;
+      final List<List<Pattern>> patternLists = new LinkedList<>();
+      String partName = patternEntryJsonElement.getKey();
+      JsonArray partPatternsJsonArray = getPatternsForPartJsonElements(partName, patternsJsonObjectInBar);
+      for (JsonElement jsonPatterns : partPatternsJsonArray) {
+        String patternName = jsonPatterns.getAsString();
+        try (Context ignored2 = context($(REFERENCING_JSON_NODE, jsonPatterns))) {
+          patternLists.add(Bar.createPatterns(patternName, bar.patterns, bar.noteMaps));
+        }
+      }
+      patternListsMap.put(partName, patternLists);
+    }
+    return patternListsMap;
+  }
+
+  private static List<String> resolveLabelsForBar(JsonObject barJsonObject) {
+    if (CompatJsonUtils.hasPath(barJsonObject, Keyword.$labels)) {
+      return StreamSupport.stream(CompatJsonUtils.asJsonArray(barJsonObject, Keyword.$labels).spliterator(), false)
+          .map(CompatJsonUtils::asString)
+          .toList();
+    }
+    return emptyList();
   }
 
   private static Groove resolveGrooveForBar(JsonObject barJsonObject, Map<String, Groove> grooves) {
@@ -95,10 +163,10 @@ public class Bar {
     return g;
   }
 
-  private Fraction resolveBeatsForBar(JsonObject barJsonObject) {
+  private static Fraction resolveBeatsForBar(JsonObject barJsonObject) {
     Fraction beats;
     try {
-      beats = Fraction.parseFraction(CompatJsonUtils.asString(barJsonObject, Keyword.$beats));
+      beats = parseFraction(CompatJsonUtils.asString(barJsonObject, Keyword.$beats));
     } catch (FractionFormatException e) {
       throw illegalFormatException(asJsonElement(barJsonObject, Keyword.$beats), FRACTION_EXAMPLE);
     }
@@ -127,17 +195,19 @@ public class Bar {
    * In the `patternNames` variable name pattern names are joined by `;`.
    *
    * @param patternNames A string that holds pattern names.
+   * @param patterns1
+   * @param noteMaps1
    * @return
    */
-  private List<Pattern> createPattern(String patternNames) {
+  private static List<Pattern> createPatterns(String patternNames, Map<String, Pattern> patterns1, Map<String, NoteMap> noteMaps1) {
     List<Pattern> patternList;
     if (patternNames.startsWith("$inline:")) {
-      patternList = singletonList(Pattern.createPattern(CompatJsonUtils.toJson(patternNames.substring("$inline:".length())).getAsJsonObject(), this.noteMaps));
+      patternList = singletonList(Pattern.createPattern(CompatJsonUtils.toJson(patternNames.substring("$inline:".length())).getAsJsonObject(), noteMaps1));
     } else {
       patternList = new LinkedList<>();
       for (String eachPatternName : patternNames.split(";")) {
         Pattern cur;
-        cur = this.patterns.get(eachPatternName);
+        cur = patterns1.get(eachPatternName);
         if (cur == null) {
           throw patternNotFound(patternNames);
         }
@@ -145,58 +215,5 @@ public class Bar {
       }
     }
     return patternList;
-  }
-
-  /**
-   * Returns a set of part names.
-   * @return A set of part names.
-   */
-  public Set<String> partNames() {
-    return Collections.unmodifiableSet(this.patternLists.keySet());
-  }
-
-  /**
-   * Returns a list of a list of patterns for the `partName`.
-   * TODO
-   *
-   * @param partName A part name for which patterns should be returned.
-   * @return A list of a list of patterns.
-   */
-  public List<List<Pattern>> part(String partName) {
-    return Collections.unmodifiableList(this.patternLists.get(partName));
-  }
-
-  public Fraction beats() {
-    return this.beats;
-  }
-
-  public Groove groove() {
-    return this.groove;
-  }
-
-  public JsonElement lookUpJsonNode(String partName) {
-    try {
-      return asJsonElement(this.barJsonObject, Keyword.$parts, partName);
-    } catch (JsonInvalidPathException e) {
-      return null;
-    }
-  }
-
-  /**
-   * Returns a root JSON object to which this bar belongs.
-   *
-   * @return A root JSON object.
-   */
-  public JsonObject rootJsonObject() {
-    return this.rootJsonObject;
-  }
-
-  /**
-   * Returns labels attached to this bar object.
-   *
-   * @return A list of labels.
-   */
-  public List<String> labels() {
-    return this.labels;
   }
 }
