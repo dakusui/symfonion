b1b8c0eaca0550be47333bcde4bbc6a6ddefdf47
Hiroshi Ukai <dakusui@users.noreply.github.com>
2025-01-26 11:29:24 +0900

    Hotfix java doc errors (#55)
    * Hotfix: Fix (and workaround) JavaDoc errors.

--- src/main/java/com/github/dakusui/symfonion/cli/subcommands/LogiasUtils.java
+++ src/main/java/com/github/dakusui/symfonion/cli/subcommands/LogiasUtils.java
@@ -3,12 +3,16 @@ package com.github.dakusui.symfonion.cli.subcommands;
 import com.github.dakusui.logias.lisp.Context;
 
 /**
- * //@formatter:off
- * //@formatter:on
+ * A utility class to access the **Logias** library, which builds and executes S-expressions using **JSON**.
  */
 public enum LogiasUtils {
   ;
 
+  /**
+   * Creates a `Context` object of **Logias** library.
+   *
+   * @return A new context object.
+   */
   public static Context createLogiasContext() {
     return Context.ROOT.createChild();
   }
